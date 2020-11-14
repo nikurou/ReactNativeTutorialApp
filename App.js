36ba1d514f99bca7160c6f86f5da0de1f64d3ff5
Dylan Chung <dylan.chung1997@gmail.com>
2020-11-13 22:02:24 -0800

    Build Navigation Stack

--- App.js
+++ App.js
@@ -1,13 +1,24 @@
+import "react-native-gesture-handler";
 import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 import ImageScreen from "./app/screens/ImageScreen";
-
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 
+const Stack = createStackNavigator();
+
 export default function App() {
-  return <WelcomeScreen />;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={WelcomeScreen} />
+        <Stack.Screen name="ImageScreen" component={ImageScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
 //To Start Server run command 'npm start'
 //CTRL + M for developer menu
 
-//10:22
+//10:22c
