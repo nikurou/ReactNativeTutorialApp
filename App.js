caf40ebd3ed0d546ed22384814e7f1a1640529a8
Dylan Chung <dylan.chung1997@gmail.com>
2020-11-22 21:03:41 -0800

    imported React Native Paper

--- App.js
+++ App.js
@@ -6,19 +6,23 @@ import ImageScreen from "./app/screens/ImageScreen";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 import RegisterScreen from "./app/screens/RegisterScreen";
 import LoginScreen from "./app/screens/LoginScreen";
+import { Provider as PaperProvider } from "react-native-paper";
+import { Text } from "react-native";
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={WelcomeScreen} />
-        <Stack.Screen name="ImageScreen" component={ImageScreen} />
-        <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
-        <Stack.Screen name="LoginScreen" component={LoginScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <PaperProvider>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={WelcomeScreen} />
+          <Stack.Screen name="ImageScreen" component={ImageScreen} />
+          <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
+          <Stack.Screen name="LoginScreen" component={LoginScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </PaperProvider>
   );
 }
 
